411bb063e803ddf098ea6fe7ee962c8caab8d053
CrackedSkull <crackedskull@protonmail.com>
2022-12-13 22:28:04 +0400

    TPA-maintenance feat: development 1-Added flake8 and isort; 2-Code has been cleaned.

--- api_handler/products/models.py
+++ api_handler/products/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+
 class Product(models.Model):
     product_id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey('users.User', on_delete=models.CASCADE)
