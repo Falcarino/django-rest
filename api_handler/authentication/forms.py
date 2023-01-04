978937594bc7ee9f4871c68d1d446fbb270935e8
ftlchimney <ftlchimney@gmail.com>
2023-01-04 16:14:31 +0400

    feat(UI): implemented css for login and register pages

--- api_handler/authentication/forms.py
+++ api_handler/authentication/forms.py
@@ -2,8 +2,12 @@ from django import forms
 
 
 class SignInForm(forms.Form):
-    email = forms.CharField(label='Email', max_length=100)
-    password = forms.CharField(label='Password', max_length=64)
+    email = forms.CharField(label='Email',
+                            max_length=100,
+                            widget=forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'Email'}))
+    password = forms.CharField(label='Password',
+                               max_length=64,
+                               widget=forms.TextInput(attrs={'class': 'input-field', 'placeholder': 'Password'}))
 
 
 class SignUpForm(forms.Form):
